48895bf6074daed1077851f3cb66c7fe393fe9f5
Julio <juliointelek@hotmail.com>
2018-08-18 09:51:17 +0200

    añadido el titulo a los enlaces view details

--- js/main.js
+++ js/main.js
@@ -195,6 +195,7 @@ createRestaurantHTML = (restaurant) => {
   const more = document.createElement('a');
   more.innerHTML = 'View Details';
   more.href = DBHelper.urlForRestaurant(restaurant);
+  more.title = restaurant.name;
   li.append(more)
 
   return li
